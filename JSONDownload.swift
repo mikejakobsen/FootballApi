5182016b3924e5953faa8b75344b13e56953edbd
Mike Jakobsen <mike@jakobsen.dk>
2016-10-07 12:01:57 +0200

    Binary to Json .·´¯`(>▂<)´¯`·.

--- JSONDownload.swift
+++ JSONDownload.swift
@@ -21,11 +21,14 @@ class JSONDownload {
             URLSession.shared.dataTask(with: request) {(data, response, error) in
                 
                 do {
-                    
+                    guard let data = data else {
+                        throw JSONError.NoData
+                    }
+                    guard let json = try JSONSerialization.jsonObject(with: <#T##Data#>, options: <#T##JSONSerialization.ReadingOptions#>)
                 } catch let error as JSONError {
                     print(error.rawValue)
-                } catch let error as Error {
-                    print(error.debugDescription)
+                } catch let error {
+                    print(error.localizedDescription)
                 }
                 
             }
